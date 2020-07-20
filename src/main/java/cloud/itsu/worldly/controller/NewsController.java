f08c9bd08305c991ea93403990c4e81e42f208c9
ronnie-xavier <ronnie-xavier@users.noreply.github.com>
2020-07-20 22:51:13 +0800

    Correct News Controller class name

--- src/main/java/cloud/itsu/worldly/controller/NewsController.java
+++ src/main/java/cloud/itsu/worldly/controller/NewsController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class newsController {
+public class NewsController {
 
     @GetMapping("/news/home-page")
     public String get() {
